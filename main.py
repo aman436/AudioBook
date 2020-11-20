6772d406d9fa8cf7d302a9a44b758e60cf39f064
aman436 <www.amankawre36@gmail.com>
2020-11-20 13:04:55 +0530

    Modified code

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import pyttsx3
 import PyPDF2
-book = open('oop.pdf', 'rb')
+book = open('test.pdf', 'rb') ##binary reading
 pdfReader = PyPDF2.PdfFileReader(book)
 pages = pdfReader.numPages
 
 speaker = pyttsx3.init()
-for num in range(7, pages):
+speaker.setProperty('rate',70)  ##to set the frequency
+voices=speaker.getProperty('voices')
+speaker.setProperty('voices',voices[0].id)
+
+for num in range(27, pages):   ## manually insert the page no. of pdf to start reading from the desired page
     page = pdfReader.getPage(num)
     text = page.extractText()
     speaker.say(text)
     speaker.runAndWait()
+~                             
